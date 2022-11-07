586b742e49c641b59a70e32e5e7ce4f042ae41ee
guido pascualetti <pascualettiguido@gmail.com>
2022-11-07 10:42:38 -0300

    Footer Accordion

---
+++ src/components/UI/Acordion.jsx
@@ -0,0 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState } from "react";
+import "./Acordion.css";
+export default function Acordion({ header, children }) {
+  const [toggleIsOpen, setToggleIsOpen] = useState(false);
+  return (
+    <>
+      <Container className="acordion">
+        <Row className=" acordion">
+          <Col
+            xs={12}
+            className="d-flex justify-content-between align-items-center border-bottom"
+            style={{ height: "50px" }}
+            onClick={() => setToggleIsOpen((prev) => !prev)}
+          >
+            <div className="d-flex align-items-center">
+              <h5 className="psico-h1  m-0">{header}</h5>
+            </div>
+
+            <div className="d-flex align-items-center">
+              {toggleIsOpen ? <i> - </i> : <i> + </i>}
+            </div>
+          </Col>
+          <Col
+            className={`${
+              toggleIsOpen ? "altura-auto" : "altura-0"
+            } acordion pt-3`}
+          >
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import logo from "../../assets/logos/logo-blanco.png";
-
+import Acordion from "../UI/Acordion";
 export default function Footer() {
   const links = {
     Estudiantes: [
@@ -23,17 +23,25 @@ export default function Footer() {
   };
   return (
     <>
-      <Container fluid className="bg-gris-oscuro text-white">
+      <Container fluid className="bg-gris-oscuro text-white pt-5 pb-5">
         <Container>
           <Row>
-            {Object.keys(links).map((k) => (
+            {/* {Object.keys(links).map((k) => (
               <Col xs={12} lg={3} key={k} className="pt-3">
                 <h5 className="pb-3">{k}</h5>
                 {links[k].map((link) => (
                   <p key={link.label}>{link.label}</p>
                 ))}
               </Col>
+            ))} */}
+            {Object.keys(links).map((k) => (
+              <Acordion key={k} header={k}>
+                {links[k].map((link) => (
+                  <p key={link.label}>{link.label}</p>
+                ))}
+              </Acordion>
             ))}
+
             <Col xs={12} lg={3} className="pt-3 order-lg-first">
               <img className="" style={{ width: "200px" }} src={logo} />
               <p className="footer-p pt-3">
