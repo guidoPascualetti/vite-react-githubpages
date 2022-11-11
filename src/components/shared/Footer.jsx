c060705d09ad0a9bbe0731d57ebbe4dcd325e092
guido pascualetti <pascualettiguido@gmail.com>
2022-11-11 12:52:21 -0300

    Navbar mobile

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,8 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import logo from "../../assets/logos/logo-blanco.png";
-import Acordion from "../UI/Acordion";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import logo from "../../assets/logos/logo-blanco.png";
+// import Acordion from "../UI/Acordion";
 export default function Footer() {
   const links = {
     Estudiantes: [
@@ -25,33 +25,7 @@ export default function Footer() {
     <>
       <Container fluid className="bg-gris-oscuro text-white pt-5 pb-5">
         <Container>
-          <Row>
-            {/* {Object.keys(links).map((k) => (
-              <Col xs={12} lg={3} key={k} className="pt-3">
-                <h5 className="pb-3">{k}</h5>
-                {links[k].map((link) => (
-                  <p key={link.label}>{link.label}</p>
-                ))}
-              </Col>
-            ))} */}
-            {Object.keys(links).map((k) => (
-              <Acordion key={k} header={k}>
-                {links[k].map((link) => (
-                  <p key={link.label}>{link.label}</p>
-                ))}
-              </Acordion>
-            ))}
-
-            <Col xs={12} lg={3} className="pt-3 order-lg-first">
-              <img className="" style={{ width: "200px" }} src={logo} />
-              <p className="footer-p pt-3">
-                Calle 51 entre 123 y 124, Ensenada (CP1925), <br />
-                Provincia de Buenos Aires, Argentina. <br />
-                <b>Teléfonos:</b> +54 (0221) 482 4415 - 482 5931 -<br /> 482
-                8457 - 483 3933 - 483-4871
-              </p>
-            </Col>
-          </Row>
+          <h1>hola</h1>
         </Container>
       </Container>
     </>
